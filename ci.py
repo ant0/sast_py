749ae637beba7b2dd30d987fc1018b45c8c3371e
Anthony Low <ant0@users.noreply.github.com>
2023-11-27 13:37:04 +0000

    Update ci.py

--- ci.py
+++ ci.py
@@ -2,7 +2,13 @@ import subprocess
 
 API_KEY='password123'
 PASSWORD='catchme'
+DB_PASSWORD='db-pass'
 
 ip = input("Enter the IP: ")
 output = subprocess.check_output(f"ping {ip}", shell=True, encoding='UTF-8')
 print(output)
+
+if 'output' in locals():
+  print(API_KEY)
+  print(PASSWORD)
+  prin(DB_PASSWORD)
